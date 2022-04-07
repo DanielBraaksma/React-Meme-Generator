2512212f560c240471ccbb3be16122504604fb91
Daniel Braaksma <danebraaksma@hotmail.com>
2022-04-07 11:00:25 -0400

    added header, form , and responsive css

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+export default function Navbar (){
+    return (
+        <header>
+            <div className="logo-container">
+                <img className="logo-pic" src="./images/Troll Face.png" />
+                <h1>Meme Generator</h1>
+            </div>
+            <h4 className="title">React - Project 3</h4>
+        </header>
+    )
+}
